957d26b8df0384b926d48299c7b86a0dd9a183a0
InfinitydeGuzma <>
2021-05-03 16:45:21 +0000

    Changed style, added index.html and script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Infinity de Guzman All rights reserved
+//
+// Created by Infinity de Guzman
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // This function displays, "Hello, World!"
+
+  document.getElementById('hello-world').innerHTML = 'Hello, World!'
+}
